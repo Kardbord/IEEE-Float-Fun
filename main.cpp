98df5f1a12721638cec553d454538b086d20c5e1
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-03-09 15:39:14 -0700

    IEEEFloat::computeExponentBits tested successfully; implementation is correct

--- main.cpp
+++ main.cpp
@@ -5,16 +5,13 @@
 
 int main() {
 
-    BaseTwoSciNumber test(2, 1.36);
-
-    BaseTwoSciNumber test1(0.085);
-
-    /*try{
-        std::string test = IEEEFloat::toFloat(53, 0.214579);
+    try{
+        std::string test = IEEEFloat::toFloat(0.085);
         std::cout << test << std::endl;
     } catch (const char* msg){
         std::cerr << msg;
         return EXIT_FAILURE;
-    }*/
+    }
+
     return EXIT_SUCCESS;
 }
